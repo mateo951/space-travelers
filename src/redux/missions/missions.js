886f5cce9febb85d52378c5c21dd4ae27a592752
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-10-11 16:59:18 -0500

    Add redux store config

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,18 @@
+const GET_MISSIONS = 'spaceTravelers/missions/GET_MISSIONS'
+
+export const getMissions = (payload) => ({
+  type: GET_MISSIONS,
+  payload
+})
+
+const reducer = (state = 10, action) => {
+  switch (action.payload) {
+    case GET_MISSIONS:
+      return { ...state }
+      break;
+    default:
+      return state;
+  }
+}
+
+export default reducer;
