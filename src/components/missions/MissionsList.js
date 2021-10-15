d4ee0f6180a573aaaba32f33af0976ac489ce263
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-10-15 13:37:35 -0500

    Add small fix to table headers

--- src/components/missions/MissionsList.js
+++ src/components/missions/MissionsList.js
@@ -23,7 +23,7 @@ const Missions = () => {
             <th>Mission</th>
             <th>Description</th>
             <th>Status</th>
-            <th>.</th>
+            <th>Action</th>
           </tr>
           {
             missions.map((mission) => (
