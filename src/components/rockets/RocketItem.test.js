1ac669ec341af062cb953d38397597628ce1b1ba
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-10-15 11:09:26 -0500

    Add fix for jest errors

--- src/components/rockets/RocketItem.test.js
+++ src/components/rockets/RocketItem.test.js
@@ -3,8 +3,26 @@ import { Provider } from 'react-redux';
 import store from '../../redux/configureStore';
 import RocketItem from './RocketItem';
 
-test('renders learn react link', () => {
-  render(<Provider store={store}><RocketItem /></Provider>);
-  const linkElement = screen.getByText(/Reserve Rocket/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const rocket = {
+  id: 32847893,
+  rocket_name: 'Rocket 99',
+  description: 'This is a description',
+  flickr_images: ['imageURL', 'imageURL2', 'imageURL3'],
+  reserved: false,
+}
+
+describe('Test rendering', () => { 
+  test('renders learn react link', () => {
+    render(
+      <Provider store={store}><RocketItem
+        key={rocket.id}
+        id={rocket.id}
+        rocketName={rocket.rocket_name}
+        description={rocket.description}
+        flickrImages={rocket.flickr_images}
+        reserved={rocket.reserved} />
+      </Provider>);
+    const linkElement = screen.getByText(/Reserve Rocket/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+})
